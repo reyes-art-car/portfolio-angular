f1e05a120d59f8b7818ebc0fce01ea21556f94e7
Maria Reyes Artacho Carrero <mariareyesartcar@gmail.com>
2026-02-12 11:55:59 +0100

    CV refactor, addition project-cards and portfolio pages

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Project } from '../../models/project.model';
 import { TruncatePipe } from '../../pipes/truncate-pipe';
 
-
 @Component({
   selector: 'app-project-card',
-imports: [TruncatePipe],
-templateUrl: './project-card.html',
+  standalone: true,
+  imports: [TruncatePipe],
+  templateUrl: './project-card.html',
   styleUrl: './project-card.css',
 })
 export class ProjectCard {
-  project!: Project;
+
+  @Input() project!: Project;
+
 }
--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -1,21 +1,42 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
-
-import { PortfolioService } from '../../services/portfolio';
-import { Project } from '../../models/project.model';
+import { RouterLink } from '@angular/router';
 import { ProjectCard } from '../../components/project-card/project-card';
+import { Project } from '../../models/project.model';
 
 @Component({
+  selector: 'app-portfolio',
   standalone: true,
-  imports: [NgFor, NgIf, ProjectCard],
-  templateUrl: './portfolio.html'
+  imports: [RouterLink, ProjectCard],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.css',
 })
 export class Portfolio {
 
-  projects: Project[] = [];
+  chefpro: Project = {
+    title: 'ChefPro',
+    description:
+      'Plataforma full-stack para reserva de menús y servicios de chefs. Backend en Spring Boot y frontend en Angular con APIs REST y autenticación.',
+    imageUrl: 'img/api.png',
+    tags: ['Angular', 'Spring Boot', 'REST'],
+    link: 'https://github.com/varelaiglesiascarmen/ChefPro',
+  };
 
-  constructor(private portfolioService: PortfolioService) {
-    this.projects = this.portfolioService.getProjects();
-  }
+  aetasHistoria: Project = {
+    title: 'Aetas Historia',
+    description:
+      'Aplicación Java con Swing para crear, editar y eliminar documentos históricos.',
+    imageUrl: 'img/icons8-java.svg',
+    tags: ['Java', 'Swing'],
+    link: 'https://github.com/reyes-art-car/aetas-historia',
+  };
+
+  nctEspana: Project = {
+    title: 'NCT España',
+    description:
+      'Aplicación web para la fanbase de NCT en España desarrollada con HTML y CSS.',
+    imageUrl: 'img/html-5.png',
+    tags: ['HTML', 'CSS'],
+    link: 'https://github.com/reyes-art-car/nct-espana.es',
+  };
 
 }
